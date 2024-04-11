f14928e9709800149d23ec7384e92f9cb7710019
Mona Lisa <joorysalam786@gmail.com>
2024-04-11 10:41:28 +0530

    updated bishop moves

--- src/arbiter/getMoves.jsx
+++ src/arbiter/getMoves.jsx
@@ -34,7 +34,7 @@ export function getKnightMoves({ position, rank, file }) {
   const moves = [];
   const enemy = position[rank][file].startsWith("w") ? "b" : "w";
 
-  const candidates = [
+  const direction = [
     [-2, -1],
     [-2, 1],
     [-1, -2],
@@ -43,10 +43,42 @@ export function getKnightMoves({ position, rank, file }) {
     [2, -1],
     [2, 1],
   ];
-  candidates.forEach((c) => {
+  direction.forEach((c) => {
     const cell = position?.[rank + c[0]]?.[file + c[1]];
     if (cell !== undefined && (cell.startsWith(enemy) || cell === "")) {
       moves.push([rank + c[0], file + c[1]]);
+    }
+  });
+  return moves;
+}
+
+export function getBishopMoves({ position, piece, rank, file }) {
+  const moves = [];
+  const us = piece[0]; // which color pieces is ours
+  const enemy = us === "w" ? "b" : "w";
+
+  const direction = [
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1],
+  ];
+
+  direction.forEach((dir) => {
+    for (let i = 1; i < 8; i++) {
+      const x = rank + i * dir[0];
+      const y = file + i * dir[1];
+      if (position?.[x]?.[y] === undefined) {
+        break;
+      }
+      if (position[x][y].startsWith(enemy)) {
+        moves.push([x, y]);
+        break;
+      }
+      if (position[x][y].startsWith(us)) {
+        break;
+      }
+      moves.push([x, y]);
     }
   });
   return moves;
